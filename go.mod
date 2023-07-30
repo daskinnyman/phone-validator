bc6d8919ac83fcb8be9ea7a66817a74a9c9b0536
Alex Chen <alex940131@gmail.com>
2023-07-30 11:02:39 +0000

    feat: add validator

--- go.mod
+++ go.mod
@@ -15,6 +15,7 @@ require (
 	github.com/go-playground/validator/v10 v10.14.1 // indirect
 	github.com/go-sql-driver/mysql v1.7.1 // indirect
 	github.com/goccy/go-json v0.10.2 // indirect
+	github.com/golang/protobuf v1.5.3 // indirect
 	github.com/jinzhu/inflection v1.0.0 // indirect
 	github.com/jinzhu/now v1.1.5 // indirect
 	github.com/json-iterator/go v1.1.12 // indirect
@@ -23,6 +24,7 @@ require (
 	github.com/mattn/go-isatty v0.0.19 // indirect
 	github.com/modern-go/concurrent v0.0.0-20180306012644-bacd9c7ef1dd // indirect
 	github.com/modern-go/reflect2 v1.0.2 // indirect
+	github.com/nyaruka/phonenumbers v1.1.7 // indirect
 	github.com/pelletier/go-toml/v2 v2.0.9 // indirect
 	github.com/twitchyliquid64/golang-asm v0.15.1 // indirect
 	github.com/ugorji/go/codec v1.2.11 // indirect
--- pkg/serivces/phoneNumber.go
+++ pkg/serivces/phoneNumber.go
@@ -3,6 +3,8 @@ package serivces
 import (
 	"phone_validator/pkg/repositories"
 	"phone_validator/pkg/serivces/dtos"
+
+	"github.com/nyaruka/phonenumbers"
 )
 
 // Step 1: define the struct for the service
@@ -23,5 +25,21 @@ func CreatePhoneValidatorService(repo repositories.PhoneNumberRepository) PhoneV
 
 // Step 3: implement the methods for the service
 func (p *phoneValidatorService) ValidatePhoneNumber(req dtos.ValidatePhoneNumberRequest) (bool, error) {
-	return false, nil
+	phoneNumberToValidate := req.PhoneNumber
+
+	// 1. Check if phone number is empty or spaces
+	if len(phoneNumberToValidate) == 0 || phoneNumberToValidate == " " {
+		return false, nil
+	}
+
+	// 2. parse the phone number
+	phoneNumber, err := phonenumbers.Parse(phoneNumberToValidate, "TW")
+
+	isValid := phonenumbers.IsValidNumberForRegion(phoneNumber, "TW")
+
+	if err != nil {
+		return false, err
+	}
+
+	return isValid, nil
 }
